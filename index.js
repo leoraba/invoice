4b3241ff2afa3e8c865b537d33a0b54dce1eec4b
Leonardo Rivera <leorivera_88@hotmail.com>
2019-02-26 12:23:54 -0600

    Adding nodemon and browserify scripts

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-console.log(`Running Node version: ${process.versions.node}`);
+console.log(`Running on Node version: ${process.versions.node}`);
+var dataBaseConn = new Promise((resolve, reject) => {
+    resolve(mongoose.connect('mongodb://localhost:27017/invoice', { useNewUrlParser: true, useCreateIndex: true }));
+}).then(success => {
+    const app = express();
 
-mongoose.connect('mongodb://localhost:27017/invoice', { useNewUrlParser: true, useCreateIndex: true });
+    //log in console
+    app.use(require('morgan')('dev'));
 
-const app = express();
+    //public resources to be accessed by client html/js/css/img
+    app.use(express.static(__dirname + '/public'));
 
-//log in console
-app.use(require('morgan')('dev'));
+    //authentication
+    require('./controllers/auth')(app);
 
-//public resources to be accessed by client html/js/css/img
-app.use(express.static(__dirname + '/public'));
+    //api routes
+    app.use('/api', require('./controllers/main')(app));
 
-//authentication
-require('./controllers/auth')(app);
+    //redirect to index.html page where angular will route internally
+    app.use('/*', function(req, res){
+        res.sendFile(__dirname + '/public/index.html');
+    });
 
-//api routes
-app.use('/api', require('./controllers/main')(app));
-
-//redirect to index.html page where angular will route internally
-app.use('/*', function(req, res){
-    res.sendFile(__dirname + '/public/index.html');
+    app.listen(3000);
+    console.log('Listening on port 3000!');
+}, error => {
+    console.log("Couldn't connect to MongoDB database.");
+    console.log("Exit.");
 });
-
-app.listen(3000);
-console.log('Listening on port 3000!');
